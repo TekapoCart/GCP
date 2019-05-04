118ada1b10e648b5bf4a5d1174914cfe1c4c4771
TekapoCart <44939943+TekapoCart@users.noreply.github.com>
2019-05-04 12:04:11 +0800

    Update reserve-ip.sh

--- reserve-ip.sh
+++ reserve-ip.sh
@@ -9,10 +9,18 @@ while getopts d: option
 done
 
 if [ -z "$DOMAIN" ]; then
-    echo "請輸入商店網址"
+    echo "請輸入商店網址 例如：www.yoursite.com"
     exit 1;
 fi
 
 REGION=asia-east1
 NAME=$(echo $DOMAIN-ip | tr . -)
-gcloud compute addresses create $NAME --region=$REGION
+
+CHECK_IP=$(gcloud compute addresses list | grep $NAME)
+if [ ${#CHECK_IP} -eq 0 ]; then	
+    gcloud compute addresses create $NAME --region=$REGION
+	CHECK_IP=$(gcloud compute addresses list | grep $NAME)
+fi
+
+VM_IP=$(echo $CHECK_IP | awk '{print $3;}')
+echo $VM_IP
